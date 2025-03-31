e8e241a7d895674f09e87769ecd7477fedbfbc52
CBurnham-BBB <cb604482@wne.edu>
2025-03-31 17:53:17 +0000

    Problem 2 almost done...

---
+++ Problem2/LED_button.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+#define BUTTON_PATH "sys/class/gpio/gpio46"
+#define LED_PATH "sys/class/gpio/gpio60"
+
+int readButton(){
+
+	fstream fs;
+	string path(BUTTON_PATH);
+	string pathName = path + "/value";
+	fs.open((pathName).c_str(), std::fstream::in);
+	int value; fs >> value;
+	fs.close();
+	return value;
+
+}
+
+void setLED(int value){
+
+	fstream fs;
+	string path(LED_PATH);
+	fs.open((path+"/value").c_str(), std::fstream::out);
+	cout << value << endl;
+	fs.close();
+
+}
+
+int main(int argc, char* argv[]){
+
+	while(1){
+		
+		int status = readButton();
+		if(status==1){
+			setLED(1);
+		} else {
+			setLED(0);
+		}
+	
+	}
+
+	return 0;
+
+}
